06f98bdeaa639232f578a836ed1838f3191d0ef0
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-22 15:07:54 +0100

    feat: create controller funcionts

--- routers/posts.js
+++ routers/posts.js
@@ -1,73 +1,26 @@
 const express = require("express")
 const router = express.Router()
 
-// IMPORTAZIONE ARRAY POSTS
-const postsData = require("../allPosts")
+// IMPORTANZIONE FUNZIONI CONTROLLER
+const controller = require("../controllers/postsController")
+
 
 // Index
-router.get('/', function (req, res) {
-    res.json(`Lista dei post`); 
-});
+router.get('/', controller.index);
 
 // Show
-router.get('/:id', function (req, res) {
-    if(isNaN(req.params.id)){
-        return res.sendStatus(400)
-    }
-
-    const post = postsData.find((elm) => elm.id == req.params.id)
-
-    if (!post){
-       res.sendStatus(404)
-    }   
-    res.json(`Dettagli di un post ${req.params.id}`);
-});
+router.get('/:id', controller.show);
 
 // Create
-router.post('/', function (req, res) {
-    res.json(`Creazione nuovo post`);
-});
+router.post('/', controller.create);
 
 // Update
-router.put('/:id', function (req, res) {
-    if(isNaN(req.params.id)){
-        return res.sendStatus(400)
-    }
-
-    const post = postsData.find((elm) => elm.id == req.params.id)
-
-    if (!post){
-       res.sendStatus(404)
-    } 
-    res.json(`Modifica integrale del post ${req.params.id} `);
-});
+router.put('/:id', controller.update);
 
 // Modify
-router.patch('/:id', function (req, res) {
-    if(isNaN(req.params.id)){
-        return res.sendStatus(400)
-    }
+router.patch('/:id', controller.update);
 
-    const post = postsData.find((elm) => elm.id == req.params.id)
-
-    if (!post){
-       res.sendStatus(404)
-    } 
-    res.json(`Modifica parziale del post ${req.params.id} `);
-});
-
-// Delete
-router.delete('/:id', function (req, res) {
-    if(isNaN(req.params.id)){
-        return res.sendStatus(400)
-    }
-
-    const post = postsData.find((elm) => elm.id == req.params.id)
-
-    if (!post){
-       res.sendStatus(404)
-    } 
-    res.json(`Eliminazione del post ${req.params.id} `);
-});
+// Destroy
+router.delete('/:id', controller.destroy);
 
 module.exports = router
