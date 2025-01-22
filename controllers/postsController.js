9963fd14aa32e9f5ad7b629953ae8ef07ff1918f
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-22 15:33:56 +0100

    feat: show uotput

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -17,7 +17,7 @@ const show = function (req, res,) {
     if (!post){
        res.sendStatus(404)
     }   
-    res.json(`Dettagli di un post ${req.params.id}`);
+    res.json(post);
 }
 
 // CREATE
